6a93ca2618863749ac80c7e06a0ed5a9d7ba967b
2yashaa@gmail.com <yasha2yashaa@gmail.com>
2021-08-25 03:00:18 +0200

    Added static about section

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
 // click active toggler
 window.addEventListener('DOMContentLoaded', (event) => {
   scrollFunction();
-    $(".forlater").click(function() {
-        if(!$(this).hasClass('active')) {
-            $(this).toggleClass('active');
-            that = this;
-            setTimeout(() => { $(this).toggleClass('active'); }, 300);
-        }
-    })
+
+  $(".nav-item-custom").click(function() {
+    console.log(this);
+    if(!$(this).hasClass('clicked')) {
+        $(this).toggleClass('clicked');
+        that = this;
+        setTimeout(() => { $(this).toggleClass('clicked'); }, 400);
+    }
+  })
+    // $(".forlater").click(function() {
+    //     if(!$(this).hasClass('active')) {
+    //         $(this).toggleClass('active');
+    //         that = this;
+    //         setTimeout(() => { $(this).toggleClass('active'); }, 300);
+    //     }
+    // })
 });
 
 // scrolled to top detector
@@ -15,7 +24,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     $("#main-nav").addClass("not-top"); // not top
+    $("#main-nav").removeClass("top");
   } else {
     $("#main-nav").removeClass("not-top"); // top
+    $("#main-nav").addClass("top");
   }
 }
